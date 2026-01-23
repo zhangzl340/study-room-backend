ceb500eb6c849609c8bb6e5f82a491a2fadfcf4c
“zhangzl340” <1065773439@qq.com>
2026-01-23 16:19:16 +0800

    swagger UI、api文档，Knife4j 可以进入

--- src/main/java/com/studyroom/common/config/WebMvcConfig.java
+++ src/main/java/com/studyroom/common/config/WebMvcConfig.java
@@ -9,6 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 /**
  * Spring MVC配置
  */
+
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
@@ -17,6 +18,19 @@ public class WebMvcConfig implements WebMvcConfigurer {
      */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//         配置Swagger UI资源（SpringDoc）
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/")
+                .setCachePeriod(0);
+// 2. Swagger UI核心资源（适配SpringDoc）
+        registry.addResourceHandler("/swagger-ui/**", "/swagger-ui.html", "/swagger-config")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/springdoc-openapi-ui/")
+                .setCachePeriod(0);
+        // 配置Knife4j文档资源
+        registry.addResourceHandler("/doc.html")
+                .addResourceLocations("classpath:/META-INF/resources/")
+                .setCachePeriod(0);
+
         // 配置上传文件访问路径
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:uploads/");
